f655d2b2f703169037a01ab631e35bdc1dff8b07
deB4SH <git@b4sh.de>
2022-12-27 11:09:06 +0100

    add: build.sh to build dev-containers

---
+++ build.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+# defaults
+REGISTRY="ghcr.io/deb4sh"
+# parse options
+while getopts :r: flag
+do
+    # shellcheck disable=SC2220
+    case "${flag}" in
+        r) REGISTRY=${OPTARG};;
+    esac
+done
+# get current tag information
+IS_DEV_BUILD=$(git tag -l --contains HEAD)
+GIT_TAG=$(git describe --abbrev=0 --tags HEAD)
+
+if [ -z "$IS_DEV_BUILD" ]
+then
+    TIMESTAMP=$(date +%s)
+    TAG=$(echo "$GIT_TAG"-"$TIMESTAMP")
+else
+    TAG=$GIT_TAG
+fi
+
+echo "Building image with tag $TAG"
+
+docker \
+    build . \
+    -f ./Dockerfile \
+    -t $(echo "$REGISTRY/rppc:$TAG")
